d30b34b3001c1469ad48c92549d8740ac43ac201
Cristian Michel <cristianrocker93@gmail.com>
2015-12-26 19:08:33 -0600

    Mapa agregado
    Ya funciona el mapa, y ya se posiciona en la region donde el usuario se
    encuentra actualmente

--- Geolevent/app/controllers/index.js
+++ Geolevent/app/controllers/index.js
@@ -2,4 +2,24 @@ function doClick(e) {
     alert($.label.text);
 }
 
+Titanium.Geolocation.getCurrentPosition(function(e)
+{
+    if (e.error)
+    {
+        alert('Error en la captura de tu ubicacion actual');
+        return;
+    }
+
+  
+var region = {
+    latitude : e.coords.latitude,
+    latitudeDelta : "0.01",
+    longitude : e.coords.longitude,
+    longitudeDelta : "0.01"
+};
+
+   $.mapview.setRegion(region);
+
+});
+
 $.index.open();
